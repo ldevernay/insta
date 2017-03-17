4b15f71ffedac1f1987d575d5f1d5e3363cab320
ldevernay <boblehamster@yahoo.fr>
2017-03-17 12:18:11 +0100

    shortid implemented

--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Photo from './Photo';
 
+var shortid = require('shortid');
+
 var PhotoGallery = React.createClass({
 
   render: function() {
 
     var photos = this.props.photos.map(function(photo) {
-      return <Photo src={photo.url} caption={photo.caption} />
+      return <Photo key={shortid.generate()} src={photo.url} caption={photo.caption} />
     });
 
     return (
